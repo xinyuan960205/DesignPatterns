7b40206a7da6800d3be157fd620a3fdc5b88f567
xinyuan <1262090875@qq.com>
2018-09-23 22:14:47 +0800

    github测试一下代码的提交

--- src/com/designPattren/demo/demo.java
+++ src/com/designPattren/demo/demo.java
@@ -3,5 +3,6 @@ package com.designPattren.demo;
 public class demo {
     public static void main(String[] args){
         System.out.println("hello world");
+        System.out.println("hello github");
     }
 }
